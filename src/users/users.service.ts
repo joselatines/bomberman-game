07f3779ca00c4cb463a5361c41e15c9aba643ec5
joselatines <joselatines33@gmail.com>
2023-07-23 12:54:13 -0400

    feat(auth): login and sign up user with mongoose connection

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema/users.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private usersModel: Model<UserDocument>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const userCreated = await this.usersModel.create(createUserDto);
+
+    return userCreated;
   }
 
   findAll() {
